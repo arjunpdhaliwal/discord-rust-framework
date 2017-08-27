91578697d0d429f85aff0c21ac23473498531564
arjunpdhaliwal <arjun.p.dhaliwal@gmail.com>
2017-08-27 20:09:27 +0000

    it supports concurrency i guess

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate websocket;
 extern crate futures;
 extern crate tokio_core;
+extern crate tokio_pool;
 
 #[macro_use]
 extern crate serde_derive;
@@ -17,7 +18,10 @@ use futures::future::Future;
 use futures::Stream;
 
 use websocket::message::OwnedMessage;
+
 use std::collections::BinaryHeap;
+use std::sync::Mutex;
+use std::sync::Arc;
 
 
 mod test;
@@ -27,35 +31,49 @@ const DISCORD_GATEWAY_URL: &str = "wss://gateway.discord.gg/?v=6&encoding=json";
 
 pub struct Client {
     core: tokio_core::reactor::Core,
-    message_queue: BinaryHeap<gateway::ClientMessage>,
+    pool: Arc<tokio_pool::TokioPool>,
+    join: Arc<tokio_pool::PoolJoin>,
+    message_queue: Arc<Mutex<BinaryHeap<gateway::ClientMessage>>>,
 }
 
 impl Client {
     pub fn new() -> Client {
         let core = Core::new()
                        .expect("Could not instantiate Client.");
-        let message_queue = BinaryHeap::<gateway::ClientMessage>::new();
+        let message_queue = Arc::new(Mutex::new(BinaryHeap::<gateway::ClientMessage>::new()));
+        let (pool, join) = tokio_pool::TokioPool::new(2)
+                                     .expect("Could not create thread pool.");
+
         Client {
             core,
+            pool: Arc::new(pool),
+            join: Arc::new(join),
             message_queue,
         }
     }
 
     pub fn authenticate(&mut self, token: String) {
+        let queue_ref1 = &self.message_queue;
+        let queue_ref2 = &self.message_queue;
+
         let socket = websocket::ClientBuilder::new(DISCORD_GATEWAY_URL)
             .expect("Could not construct client.")
             .add_protocol("rust-websocket")
             .async_connect(None, &(&mut self.core).handle())
             .and_then(|(duplex, _)| {
-                let (sink, stream) = duplex.split();
-                let a = stream.filter_map(|message| Client::handle_stream(message, token.clone())); //FIXME: do i really need to clone this?
-                a.forward(sink)
-        });
-        self.core.run(socket)
-                 .expect("Could not connect to websocket.");
+                    Client::gateway_autenticate(token.clone(), queue_ref1);
+                    let (sink, stream) = duplex.split();
+                    let a = stream.filter_map(|message| Client::handle_stream(message, queue_ref2));
+                    a.forward(sink)
+            });
+        
+
+        self.core.run(socket);
     }
 
-    fn handle_stream(message: websocket::OwnedMessage, token: String) -> Option<websocket::OwnedMessage> {
+    fn gateway_autenticate(token: String, message_queue: &Arc<Mutex<BinaryHeap<gateway::ClientMessage>>>) {
+        let mut queue = message_queue.lock().unwrap();
+
         let mut properties = BTreeMap::new();
         properties.insert(String::from("$os"), String::from("Linux"));
 
@@ -78,6 +96,12 @@ impl Client {
                                  .expect("Could not serialize response."),
             priority: 0,
         };
+
+        queue.push(identification_message);
+    }
+
+    fn handle_stream(message: websocket::OwnedMessage, message_queue: &Arc<Mutex<BinaryHeap<gateway::ClientMessage>>>) -> Option<websocket::OwnedMessage> {
+        let mut queue = message_queue.lock().unwrap();
 
         if let OwnedMessage::Text(text) = message {
             let deserialized_dispatch: gateway::ServerMessage = serde_json::from_str(&text)
@@ -104,8 +128,11 @@ impl Client {
                     let deserialized_data: gateway::hello::Hello = serde_json::from_value(serialized_data)
                                                                       .expect("Could not parse JSON.");
                     println!("\nServer: {:#?}\n", deserialized_data);
-                    println!("\nResponse: {:#?}\n", identification_message_body);
-                    Some(OwnedMessage::Text(identification_message.body))
+
+                    let message = queue.pop()
+                                                    .expect("Could not read message from queue.");
+                    println!("\nResponse: {:#?}\n", message.body);
+                    Some(OwnedMessage::Text(message.body))
                 }
             }
         } else {
