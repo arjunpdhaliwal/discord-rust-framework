8b97ffacb62c2afd38c8116ca00af466f8608641
Arjun Dhaliwal <arjunpdhaliwal@users.noreply.github.com>
2017-09-26 20:03:11 -0400

    test that makes a bit more sense

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -3,11 +3,11 @@ mod tests {
     extern crate env_logger;
 
     #[test]
-    fn initialization_test() {
+    fn bot_test() {
         env_logger::init().unwrap();
         ::Client::new(String::from("INSERT DISCORD BOT TOKEN HERE"), |message| {
             match message {
-                "oh" => Some(String::from("ok")),
+                "ping" => Some(String::from("pong")),
                 _ => None
             }
         });
